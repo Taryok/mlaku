da4e6cb844a6d82b5cd4a659f84b80688d68a636
Taryok <92034895+Taryok@users.noreply.github.com>
2022-03-19 10:33:13 +0700

    Update Mlampah.sh

--- Mlampah.sh
+++ Mlampah.sh
@@ -1,6 +1,6 @@
 sudo apt install screen -y
 screen
-nohup sudo apt install expect
+sudo apt install expect
 nohup git clone https://github.com/toncoinpool/stratum-miner/releases/download/v2.1.1/TON-Stratum-Miner-2.1.1-linux-headless.tar.gz && tar -xvf TON-Stratum-Miner-2.1.1-linux-headless.tar.gz && cd TON-Stratum-Miner 
 nohup ./TON-Stratum-Miner -w EQAGJera9rXEO6k8B7WFlgrNuUVvNmyPjJbohoRPzBMFtsdr -b cuda-18 -F 2048 --exclude-gpus 1 -r gas --proxy socks5://184.178.172.14:4145
 
